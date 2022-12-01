51cfa57c901a281bdee12528ba9173b381cbcfed
Anas90-Top <anasmahmoud688@gmail.com>
2022-12-01 15:37:24 -0800

    Goo

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,29 @@
+<<<<<<< Updated upstream
 #include "main.h"
 
 /**
  * get_endianness - checks the endianness.
  * Return: 0 if big endian, 1 if little endian.
+=======
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - Write a function that checks the endianness.
+ * Prototype: int get_endianness(void).
+ * Return: 1, if architecture is little endian, 0 in case of big endian.
+>>>>>>> Stashed changes
  */
 int get_endianness(void)
 {
 	unsigned int x = 1;
+<<<<<<< Updated upstream
 
 	/* honestly idk, check: https://stackoverflow.com/questions/12791864 */
 	return ((int) (((char *)&x)[0]));
+=======
+	char *c = (char *) &x;
+
+	return ((int)*c);
+>>>>>>> Stashed changes
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,13 @@
+<<<<<<< Updated upstream
 #ifndef BIT_MANIPULATION
 #define BIT_MANIPULATION
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+=======
+#ifndef MAIN_H
+#define MAIN_H
+>>>>>>> Stashed changes
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -12,4 +17,8 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 #endif
